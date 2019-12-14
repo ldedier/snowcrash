e76a148255a942dbe15218f168d2156114ee0143
ldedier <dedierludovic@gmail.com>
2019-12-14 10:27:52 +0100

    level12 wip

--- level12/resources/level12.pl
+++ level12/resources/level12.pl
@@ -1,15 +1,30 @@
 #!/usr/bin/env perl
+# localhost:4646
+use CGI qw{param};
+print "Content-type: text/html\n\n";
 
 sub t {
+  $nn = $_[1];
   $xx = $_[0];
   $xx =~ tr/a-z/A-Z/;
   $xx =~ s/\s.*//;
   print $xx;
   @output = `egrep "^$xx" /tmp/xd 2>&1`;
-  print "OUTPUTT:";
-  print $output
+  foreach $line (@output) {
+      ($f, $s) = split(/:/, $line);
+      if($s =~ $nn) {
+          return 1;
+      }
+  }
+  return 0;
 }
 
-#t("\$(getflag>/tmp/okok)");
-t("\"\`+\`getflag>\\");
-#t("getflag");
+sub n {
+  if($_[0] == 1) {
+      print("..");
+  } else {
+      print(".");
+  }
+}
+
+n(t(param("x"), param("y")));
